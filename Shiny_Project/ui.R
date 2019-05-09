3cef354d126d4c0bc0575f4789fb68386f4d940d
Jonathan Blake Kirkland <jbkirkland@ucdavis.edu>
2019-05-09 20:55:36 +0000

    Header updates

--- Shiny_Project/ui.R
+++ Shiny_Project/ui.R
@@ -1,11 +1,7 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# ui.R
+# Shiny Web Project 
+# BIS 180L
+# Jana and Jonathan
 
 library(shiny)
 
