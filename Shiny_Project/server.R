4c6377d42369d741c25d7747d8cc1856cc40c527
Jonathan Blake Kirkland <jbkirkland@ucdavis.edu>
2019-05-10 14:44:14 -0700

    Graph showing, region data working!

--- Shiny_Project/server.R
+++ Shiny_Project/server.R
@@ -15,9 +15,9 @@ data.pheno.mds <- read_csv("data.pheno.mds.csv", col_names = TRUE)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   output$mainGraph <- renderPlot({ # main graph output == plottedData
-    pheno.data.filtered <- filter(data.pheno.mds, 'Region' == input$regions)
+    pheno.data.filtered <- data.pheno.mds %>% filter(Region == input$regions)
     pheno.plot <- ggplot(data = pheno.data.filtered, 
-                         aes_string(
+                         aes(
                            x = `Seed length`, # First phenotype selected
                            y = `Seed width` # Second phenotype selected
         )
