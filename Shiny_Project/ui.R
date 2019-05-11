a2d907bc356b9cb6dff632cbd54cc55362629c58
Jana Gagacheva <j.gagaceva@gmail.com>
2019-05-11 12:44:00 -0700

    working and somewhat prettier plot

--- Shiny_Project/ui.R
+++ Shiny_Project/ui.R
@@ -4,6 +4,7 @@
 # Jana and Jonathan
 
 library(shiny)
+library(dplyr)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage( #create the overall page
@@ -12,33 +13,43 @@ shinyUI(fluidPage( #create the overall page
   titlePanel("Rice Data"),
   
   # Some helpful information
-  helpText("This application creates a boxplot to show difference between",
-           "iris species.  Please use the radio box below to choose a trait",
-           "for plotting"),
+  helpText(h3("This application creates a scatter plot to show the variation and dependence between three 
+           characteristics of the Rice data. Using this Shiny app, you can manipulate and 
+           observe the dynamic relationship of some of the characteristics of the Rice plants based on 
+           region.")),
   
   # Sidebar with a radio box to input which trait will be plotted
   sidebarLayout(
     sidebarPanel(
-      radioButtons("region", #the input variable that the value will go into
-                   "Choose a region to display:",
-                   c("Europe",
-                     "E Asia",
-                     "S Asia",
-                     "Pacific", "Mid East", "America", "SE Asia",
-                     "Africa", "C Asia")),
-      radioButtons("pheno1",
-                   "Choice 1",
+     selectInput("region",
+                  label = "Choose a region to display:",
+                  choices = c("Europe",
+                    "E Asia",
+                    "S Asia",
+                    "Pacific", "Mid East", "America", "SE Asia",
+                    "Africa", "C Asia")),
+      
+      # radioButtons("region", #the input variable that the value will go into
+      # "Choose a region to display:",
+      #  c("Europe",
+      #   "E Asia",
+      #   "S Asia",
+      #   "Pacific", "Mid East", "America", "SE Asia",
+      #   "Africa", "C Asia")),
+      # 
+        radioButtons("pheno1",
+                   "Seed length vs width dependent on region",
                    c("Seed_length",
                      "Seed_width")), 
-      radioButtons("pheno2",
-                   "Choice 2",
+       radioButtons("pheno2",
+                   "Seed volume vs sruface area dependent on region",
                    c("Seed_volume",
                      "Seed_surface_area"))
           
       ),
     
     # Show a plot of the generated distribution
-    mainPanel(plotOutput("plottedData")
+    mainPanel(plotOutput(outputId = "plottedData")
     )
   )
 ))
