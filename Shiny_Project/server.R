5f688276454c878306cabdb0cc570b298db42b85
Jana Gagacheva <j.gagaceva@gmail.com>
2019-05-11 12:44:43 -0700

    working and some changes for prettier plot

--- Shiny_Project/server.R
+++ Shiny_Project/server.R
@@ -12,17 +12,20 @@ library(tidyverse)
 data.pheno.mds <- read_csv("data.pheno.mds.csv", col_names = TRUE)
 
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a plot
 shinyServer(function(input, output) {
   output$plottedData <- renderPlot({ # main graph output == plottedData
     pheno.data.filtered <- data.pheno.mds %>% filter(Region == input$region)
     pheno.plot <- ggplot(data = pheno.data.filtered, 
                          aes_string(
                            x = input$pheno1, # First phenotype selected
-                           y = input$pheno2 # Second phenotype selected
+                           y = input$pheno2, # Second phenotype selected
+                           size = I(3)
+            
+                        
         )
       )
-    pheno.plot + geom_point()
+    pheno.plot + geom_point(color = "firebrick4")
   })
   
 })
