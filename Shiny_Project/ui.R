2602e1b28aa005e1e85347a7e4a0c73ac0d603a0
jgagacheva <jgagacheva@ucdavis.edu>
2019-05-13 15:50:46 +0000

    final draft

--- Shiny_Project/ui.R
+++ Shiny_Project/ui.R
@@ -1,3 +1,4 @@
+
 # ui.R
 # Shiny Web Project 
 # BIS 180L
@@ -15,7 +16,7 @@ shinyUI(fluidPage( #create the overall page
   # Some helpful information
   helpText(h3("This application creates a scatter plot to show the variation and dependence between four 
            features of the Rice data. Using this Shiny app, you can manipulate and 
-           observe the dynamic relationship of some of the features of the Rice plants based on 
+           observe their dynamic relationship based on 
            region.")),
   
   # Sidebar with a radio box to input which trait will be plotted
@@ -29,20 +30,12 @@ shinyUI(fluidPage( #create the overall page
                     "Pacific", "Mid East", "America", "SE Asia",
                     "Africa", "C Asia")),
       
-      # radioButtons("region", #the input variable that the value will go into
-      # "Choose a region to display:",
-      #  c("Europe",
-      #   "E Asia",
-      #   "S Asia",
-      #   "Pacific", "Mid East", "America", "SE Asia",
-      #   "Africa", "C Asia")),
-      # 
         radioButtons("pheno1",
-                   "Seed length vs width dependent on region",
+                   "Characteristic 1 on x-axis",
                    c("Seed_length",
                      "Seed_width")), 
        radioButtons("pheno2",
-                   "Seed volume vs sruface area dependent on region",
+                   "Characteristic 2 on y-axis",
                    c("Seed_volume",
                      "Seed_surface_area"))
           
